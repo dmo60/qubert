29c10a4477711f28d2895db33ffacecca53fc606
Phabian <fabian.nussberger@web.de>
2015-06-12 15:24:52 +0200

    update to global db

--- videoinfo.js
+++ videoinfo.js
@@ -1,21 +1,16 @@
 /**
  * Created by Fabian on 12.06.2015.
  */
-var mysql      = require('mysql');
+var database = require("./database");
+var db = database.MySqlConnection;
 
 function handleVideoInfoRequest(req, res) {
-    var connection = mysql.createConnection({
-        host     : 'mediaq.dbs.ifi.lmu.de',
-        user     : 'student',
-        password : 'tneduts',
-        database : 'MediaQ_V2'
-    });
 
     var videoID = req.query.videoID;
 
 
-    connection.query("SELECT * from VIDEO_INFO WHERE VideoId = '"+videoID+"'", function(err, rows, fields) {
-        connection.end();
+    db.query("SELECT * from VIDEO_INFO WHERE VideoId = '"+videoID+"'", function(err, rows, fields) {
+        db.end();
         if (!err) {
             //Which fields do we want/need?
             res.json({FileName:rows[0].VideoId});
