9bd31be540a99fd2032de601eacb39d76fad94f0
Alexander Klimczak <alexander.klimczak@gmail.com>
2015-06-26 14:58:07 +0200

    Routes for intersections will be drawn. Red marker = currently selected yellow marker = not selected orange marker = has an intersection with the selected video

--- frontend/qubert.js
+++ frontend/qubert.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     var url = "http://127.0.0.1:8080";
     var route;
     var markers = [];
+    var intersectionRoutes = [];
 
     function initialize() {
         var mapOptions = {
@@ -36,7 +37,7 @@ $(document).ready(function () {
                 });
                 marker.metaData = {id: this.id};
                 google.maps.event.addListener(marker, 'click', function () {
-                    drawPath(marker.metaData.id);
+                    markerClicked(marker);
                 });
                 markers.push(marker);
             });
@@ -44,12 +45,64 @@ $(document).ready(function () {
         });
 
     }
+    function removeIntersectionRoutes(){
+        for (var i = 0; i < intersectionRoutes.length; i++) {
+            intersectionRoutes[i].setMap(null);
+        }
+        routes = [];
+    }
+    function markIntersectionMarker(id) {
+        for (var i = 0; i < markers.length; i++) {
+            if (markers[i].metaData.id == id) {
+                markers[i].setIcon('https://www.google.com/mapfiles/marker_orange.png');
+            }
+        }
+    }
 
     function removeMarkers() {
         for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(null);
         }
         markers = [];
+    }
+
+    function markerClicked(marker){
+        var id = marker.metaData.id;
+        console.log("Clicked "+id);
+        drawPath(id);
+        makeOtherMarkersTransparent(marker);
+        removeIntersectionRoutes();
+        drawIntersectionRoutes(marker.metaData.id);
+    }
+
+    function makeOtherMarkersTransparent(marker){
+        marker.setIcon('https://www.google.com/mapfiles/marker.png');
+        for (var i = 0; i < markers.length; i++) {
+            if (markers[i]!=marker) {
+                markers[i].setIcon('https://www.google.com/mapfiles/marker_yellow.png');
+            }
+        }
+    }
+
+    function drawIntersectionRoutes(id){
+        $.get(getURLforIntersections(id), function (data) {
+            console.log("Drawing " + data.length +"intersecting paths");
+            $.each(data, function () {
+                var routePoints = [];
+                var coordinates = this.trajectory.coordinates;
+                markIntersectionMarker(this.VideoId);
+                $.each(coordinates, function (index,coords) {
+                    routePoints.push(new google.maps.LatLng(coords[1], coords[0]));
+                });
+                var route = new google.maps.Polyline({
+                    path: routePoints,
+                    strokeWeight:1,
+                    strokeOpacity: 0.2,
+                    map: map
+                });
+                intersectionRoutes.push(route);
+            });
+        });
     }
 
     function drawPath(id) {
@@ -76,6 +129,10 @@ $(document).ready(function () {
 
     function getURLforPath(id) {
         return url + "/videopath?videoID=" + id;
+    }
+
+    function getURLforIntersections(id) {
+        return url + "/intersections?videoID=" + id;
     }
 
 
